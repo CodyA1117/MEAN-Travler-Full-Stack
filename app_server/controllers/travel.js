ba3768715581f2b541ee8df709be9658277fd78a
CodyA1117 <adamscody079@gmail.com>
2025-06-06 08:18:52 -0500

    module 5 work, may need to edit.

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,10 +1,50 @@
 /*GET travel view*/
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
-const travel = (req, res) => {
-    res.render('travel',{title: 'Travlr Getaways'});
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
 };
+
+
+const travel = async function (req, res, next) {
+    console.log('Starting API call to fetch trips...');
+
+    await fetch(tripsEndpoint, options)
+        .then((res) => {
+            console.log('Received response from API. Now converting to JSON...');
+            return res.json();  
+        })
+        .then((json) => {
+            
+            let message = null;
+
+            if (!Array.isArray(json)) {
+                message = 'API lookup error';
+                json = [];
+            }
+
+            if (json.length === 0) {
+                message = 'No trips exist in our database!';
+            }
+
+            console.log('Rendering travel page with trips:', json.length);
+
+            res.render('travel', {
+                title: 'Travlr Getaways',
+                trips: json,
+                message
+            });
+        })
+        .catch((err) => res.status(500).send(err.message));
+};
+
+
+
 
 module.exports = {
     travel
